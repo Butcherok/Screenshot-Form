8439f9c5eefd30fed4a77c811f2d9e80b74f82f6
Butcherok <Butcherok231@gmail.com>
2024-07-10 14:58:18 +0300

    add input form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
+import {
+  Container,
+  Wrapper,
+  DataWrapper,
+  Subtitle,
+  InputWrapper,
+} from 'components/App.styled';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      ScreenShot Form
-    </div>
+    <Container>
+      <Wrapper>
+        <Subtitle>Input data</Subtitle>
+        <DataWrapper>
+          <InputWrapper>
+            <label>Instrument</label>
+            <input />
+          </InputWrapper>
+          <InputWrapper>
+            <label>Side</label>
+            <input />
+          </InputWrapper>
+          <InputWrapper>
+            <label>Price</label>
+            <input />
+          </InputWrapper>
+          <InputWrapper>
+            <label>Total size</label>
+            <input />
+          </InputWrapper>
+          <InputWrapper>
+            <label>Total volume</label>
+            <input />
+          </InputWrapper>
+          <InputWrapper>
+            <label>Date</label>
+            <input />
+          </InputWrapper>
+          <InputWrapper>
+            <label>Trade Id</label>
+            <input />
+          </InputWrapper>
+        </DataWrapper>
+      </Wrapper>
+    </Container>
   );
 };
